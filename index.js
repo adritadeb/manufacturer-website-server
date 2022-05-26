1b01090262b94db89479ffe4104c6ddfd450e6b0
adritadeb <adritad2019@gmail.com>
2022-05-26 21:42:46 +0600

    single tooli loaded

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -24,6 +24,14 @@ async function run() {
             const tools = await toolsCollection.find(query).toArray();
             res.send(tools);
         });
+
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
+        })
+
     }
     finally {
 
