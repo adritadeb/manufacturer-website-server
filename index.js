f5955cc68b175b7c332bfefa771d3dfbee27fbbc
adritadeb <adritad2019@gmail.com>
2022-05-28 21:10:54 +0600

    verify jwt done

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nkvzr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         await client.connect();
@@ -20,6 +35,7 @@ async function run() {
         const toolsCollection = client.db('roll-wall').collection('tools');
         const orderCollection = client.db('roll-wall').collection('orders');
         const reviewCollection = client.db('roll-wall').collection('reviews');
+        const userCollection = client.db('roll-wall').collection('users');
 
         app.get('/tools', async (req, res) => {
             const query = {};
@@ -46,17 +62,20 @@ async function run() {
             res.send(result);
         });
 
-        app.post('/orders', async (req, res) => {
+        app.post('/orders', verifyJWT, async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         });
 
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
             const email = req.query.email;
-            const query = { email: email };
-            const orders = await orderCollection.find(query).toArray();
-            res.send(orders);
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const orders = await orderCollection.find(query).toArray();
+                res.send(orders);
+            }
         });
 
         app.post('/reviews', async (req, res) => {
@@ -69,6 +88,21 @@ async function run() {
             const query = {};
             const orders = await reviewCollection.find(query).toArray();
             res.send(orders);
+        });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            }
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            })
+            res.send({ result, token });
         });
 
     }
